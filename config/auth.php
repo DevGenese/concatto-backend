56277d04b2534f662af28c722dc6bc8081f9422d
Andrey Nunes <nandreyout@gmail.com>
2025-02-24 01:20:42 -0300

    ajuste configs

--- config/auth.php
+++ config/auth.php
@@ -40,12 +40,6 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'api' => [
-            'driver' => 'auth_token',
-            'provider' => 'users',
-            'input_key' => 'api_token',
-            'storage_key' => 'api_token',
-        ],
     ],
 
     /*
