601acf769140df70f4baf1201aef2434fd71ba1b
Andrey Nunes <nandreyout@gmail.com>
2025-03-02 09:47:34 -0300

    ajustando schedule model

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\BelongsToManyRelationship;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Schedule extends Model
 {
@@ -18,8 +19,26 @@ class Schedule extends Model
         'finished'
     ];
 
+    protected $hidden = ['cooperative_id', 'locality_id', 'location_id'];
+    protected $appends = ['cooperative', 'location', 'locality'];
+
     public function users(): BelongsToManyRelationship
     {
         return $this->belongsToManyRelationship(User::class, 'schedule_users');
+    }
+
+    public function getLocationAttribute(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function getLocalityAttribute(): BelongsTo
+    {
+        return $this->belongsTo(Locality::class);
+    }
+
+    public function getCooperativeAttribute()
+    {
+        return $this->belongsTo(Cooperative::class)->select('name');
     }
 }
