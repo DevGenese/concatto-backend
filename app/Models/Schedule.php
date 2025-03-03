0b1aae4d78fd574a18bf857f670631af72430117
Andrey Nunes <nandreyout@gmail.com>
2025-03-03 17:26:09 -0300

    ajsutando schedule

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -43,7 +43,7 @@ class Schedule extends Model
 
     public function getLocalityAttribute()
     {
-        return $this->belongsTo(Locality::class, 'locality_id', 'id')->value('name');
+        return $this->belongsTo(Locality::class, 'locality_id', 'id');
     }
 
     public function getCooperativeAttribute()
